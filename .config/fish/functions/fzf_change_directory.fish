dfd0e0dd258614c1691fabb420a9637a950173de
kien.mac <kien.mac@niteco.se>
2026-07-04 23:38:10 +0700

    fix dir definition

--- .config/fish/functions/fzf_change_directory.fish
+++ .config/fish/functions/fzf_change_directory.fish
@@ -16,6 +16,5 @@ function fzf_change_directory
             find (ghq root) -maxdepth 4 -type d -name .git 2>/dev/null | sed 's/\/\.git//'
         end
         ls -ad */ 2>/dev/null | perl -pe "s#^#$PWD/#" | grep -v \.git
-        ls -ad $HOME/Documents/Dev/*/* 2>/dev/null | grep -v \.git
     end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
 end
